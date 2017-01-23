656ae09ee1511f64e95b3e867f28b9e5468e290c
Rayzr522 <rayzr522@gmail.com>
2017-01-23 16:18:08 -0500

    Project 'StaffLogin' generated
    Generated using PerceivePluginArchetype and custom CLI project generator tool by Rayzr

---
+++ README.md
@@ -0,0 +1,2 @@
+# StaffLogin
+Forces staff to have to log in before they can move or do anything.
---
+++ src/main/java/com/rayzr522/stafflogin/StaffLogin.java
@@ -0,0 +1,12 @@
+package com.rayzr522.stafflogin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class StaffLogin extends JavaPlugin {
+    
+    @Override
+    public void onEnable() {
+        
+    }
+    
+}
